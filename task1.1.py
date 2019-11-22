2469c9e13f81384044436ed61dc85eff6f25e621
theprogressor <57048729+theprogressor@users.noreply.github.com>
2019-11-22 11:38:22 +0300

    Add files via upload

---
+++ task1.1.py
@@ -0,0 +1,13 @@
+a = 8
+b = 'OK'
+c = 777
+d = c != 777
+ 
+print('a =', a, '  type:', type(a))
+print('b =', b, '  type:', type(b))
+print('c =', c, '  type:', type(c))
+print('d =', d, '  type:', type(d))
+e = input('Введите e: ')
+print('e =', e, '  type:', type(e))
+ 
+print('______________________')
